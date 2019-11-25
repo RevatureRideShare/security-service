77469f1f8e0b787cb17db2c971696f44d59cb6ac
Jacob <pisecojacob@gmail.com>
2019-11-25 11:09:00 -0500

    replaced contextLoads test with trivial test #5

--- src/test/java/com/revature/SecurityServiceApplicationTests.java
+++ src/test/java/com/revature/SecurityServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,8 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class SecurityServiceApplicationTests {
 
   @Test
-  void contextLoads() {
-
+  void myTest() {
+    assertTrue(true);
   }
 
 }
