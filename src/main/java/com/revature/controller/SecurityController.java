552706420054e12ee92f9ba1a77c3a9b3f07aa8c
unknown <mcbride.mik@gmail.com>
2019-12-02 12:07:39 -0500

    Removing @CrossOrigin annotation from controllers.

--- src/main/java/com/revature/controller/SecurityController.java
+++ src/main/java/com/revature/controller/SecurityController.java
@@ -24,7 +24,6 @@ import javax.ws.rs.HttpMethod;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -46,7 +45,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/")
-@CrossOrigin
 public class SecurityController {
 
   private static Logger log = Logger.getLogger("SecurityController");
